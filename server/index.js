c326fbfc983567933c731f05ed942926fdabd256
Christopher Iliyas <christopher.iliyas@andela.com>
2019-04-19 13:24:46 +0100

    finish setting up server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,17 @@
-console.log('started');
+import express from 'express';
+import logger from 'morgan';
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('*', (req, res) => res.status(200).send({
+  message: 'Testing sequelize magic functions.',
+}));
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
+
+export default app;
